f3a8d4ab6e4e21eebb28a1f53de3ec9d71d7094d
Brooks Swinnerton <bswinnerton@gmail.com>
2014-06-02 20:31:23 -0400

    Persist latitude and longitude on  model

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -8,6 +8,19 @@ class Picture < ActiveRecord::Base
   after_post_process :copy_exif_data
 
   def copy_exif_data
-  	binding.pry
+  	i = MiniExiftool.new(image.queued_for_write[:original].path)
+  	self.latitude = parse_latlong(i[:gpslatitude])
+  	self.longitude = parse_latlong(i[:gpslongitude])
+  end
+
+  def parse_latlong(latlong)
+  	latlong.scan(/(.*) deg (.*)' (.*)" (.*)/).map do |d,m,s,r|
+  		calc = d.to_f + m.to_f/60 + s.to_f/3600
+  		if ['W', 'S'].include? r
+  		  -calc
+  		else
+  		  calc
+  		end
+  	end.last
   end
 end
